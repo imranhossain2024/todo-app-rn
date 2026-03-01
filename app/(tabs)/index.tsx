3e73d44fd8fca35979a65196b59cfaf907f2fcf4
Imran775-CT <Imran775-CT@gmail.com>
2026-03-01 15:32:34 +0600

    feat: Migrate to pnpm, add initial index screen, and implement theme management with dark mode support.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -18,11 +18,11 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "red",
+    backgroundColor: "white",
     gap: 10,
   },
   content: {
-    color: "white",
+    color: "black",
     fontSize: 22,
     fontWeight: "bold",
   },
