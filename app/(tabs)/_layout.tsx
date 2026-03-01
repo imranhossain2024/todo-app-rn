f741cf93cebe55c96a7a61f6912a409b5555844c
Imran775-CT <Imran775-CT@gmail.com>
2026-03-01 14:19:16 +0600

    feat: Implement initial Expo Router navigation, add a settings screen, and configure Prettier and VS Code formatting.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,15 +1,50 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Tabs } from 'expo-router'
-import { Ionicons } from '@expo/vector-icons'
+import { View, Text } from 'react-native';
+import React from 'react';
+import { Tabs } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
 
 const TabsLayout = () => {
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Screen name="index" options={{ title: "Todos", tabBarIcon:({color,size}) => ( <Ionicons name="list" size={size} color={color} />) }} />
-      <Tabs.Screen name="settings" options={{ title: "Settings", tabBarIcon:({color,size}) => ( <Ionicons name="settings" size={size} color={color} />) }} />
-    </Tabs> 
-  )
-}
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: 'crimson',
+        tabBarInactiveTintColor: 'green',
+        tabBarStyle: {
+          backgroundColor: '#1528eeff',
+          borderTopWidth: 1,
+          borderTopColor: 'yellow',
+          height: 90,
+          paddingBottom: 30,
+          paddingTop: 10,
+        },
+        tabBarLabelStyle: {
+          fontSize: 12,
+          fontWeight: '600',
+        }
 
-export default TabsLayout
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Todos',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="flash-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: 'Settings',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="settings" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+};
+
+export default TabsLayout;
